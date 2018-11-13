68d2c0c18decfa0936d6c1da0e1465e66fa8241b
Bill McCutchan <bmccutchanjr@gmail.com>
2018-11-13 13:40:39 -0500

    GUI components

--- app/public/javascript/index.js
+++ app/public/javascript/index.js
@@ -1,4 +1,18 @@
 $(document).ready(function()
-{
+{   $("#message-wrapper").css("display", "none");
     
+    $(".signin-button").click(function(event)
+    {   // event handler for "Sign in with..." buttons
+
+        if ($(this).attr("id") === "Google")
+        {
+        }
+        else
+        if ($(this).attr("id") === "Facebook")
+        {
+        }
+
+        window.open("find.html", "_self");
+    })
+
 })
